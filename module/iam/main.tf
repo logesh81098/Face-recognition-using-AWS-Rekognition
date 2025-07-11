44465bd3cf1c47ea6daa9ced095a0bc14e38bbda
Logesh Shanmugavel <logesh81098@gmail.com>
2025-07-11 21:33:12 +0530

    Deployed IAM Role for Lambda Function to create Faceprints

--- module/iam/main.tf
+++ module/iam/main.tf
@@ -46,8 +46,8 @@ resource "aws_iam_policy" "collectionid-policy" {
         "Effect": "Allow",
         "Action": [
             "logs:CreateLogGroup",
-                "logs:CreateLogStream",
-                "logs:PutLogEvents"
+            "logs:CreateLogStream",
+            "logs:PutLogEvents"
         ],
         "Resource": "arn:aws:logs:*:*:*"
     },
@@ -78,4 +78,104 @@ EOF
 resource "aws_iam_role_policy_attachment" "collectionid" {
   role = aws_iam_role.collectionid-role.id
   policy_arn = aws_iam_policy.collectionid-policy.arn
+}
+
+
+####################################################################################################################################################################################
+#                                                                          IAM Role
+####################################################################################################################################################################################
+
+#IAM Role for Lambda function to generate faceprints from the source images store in S3 bucket
+
+resource "aws_iam_role" "rekognition-faceprints-role" {
+  name = "Rekognition-Faceprints-Role"
+  description = "IAM Role for Lambda function to generate faceprints from the source images store in S3 bucket"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+
+}  
+EOF
+  tags = {
+    Name = "Rekognition-Faceprints-Role"
+    Project = "Recognizing-faces-using-AWS-Rekognition-service"
+  }
+}
+
+
+####################################################################################################################################################################################
+#                                                                          IAM Policy
+####################################################################################################################################################################################
+
+#IAM Policy for Lambda function to generate faceprints from the source images store in S3 bucket
+
+resource "aws_iam_policy" "rekognition-faceprints-policy" {
+  name = "Rekognition-Faceprints-Policy"
+  description = "IAM Policy for Lambda function to generate faceprints from the source images store in S3 bucket"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "CloudWatchLogGroup",
+      "Effect": "Allow",
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:*:*:*"
+    },
+    {
+      "Sid": "GetItemsFromS3Bucket",
+      "Effect": "Allow",
+      "Action": [
+        "s3:GetObject",
+        "s3:HeadObject"
+      ],
+      "Resource": "arn:aws:s3:::face-rekognition-source-bucket/*"
+    },
+    {
+      "Sid": "IndexFace",
+      "Effect": "Allow",
+      "Action": [
+        "rekognition:IndexFaces"
+      ],
+      "Resource": "arn:aws:rekognition:*:*:collection/*"
+    },
+    {
+      "Sid": "PutItemsinDynamoDBTable",
+      "Effect": "Allow",
+      "Action": [
+        "dynamodb:PutItem"
+      ],
+      "Resource": "arn:aws:dynamodb:*:*:table/Faceprints-Table"
+    }
+  ]
+
+}  
+EOF
+  tags = {
+    Name = "Rekognition-Faceprints-Policy"
+    Project = "Recognizing-faces-using-AWS-Rekognition-service"
+  }
+}
+
+
+####################################################################################################################################################################################
+#                                                                     IAM Role Policy Attachment
+####################################################################################################################################################################################
+
+resource "aws_iam_role_policy_attachment" "faceprints" {
+  role = aws_iam_role.rekognition-faceprints-role.id
+  policy_arn = aws_iam_policy.rekognition-faceprints-policy.arn
 }
