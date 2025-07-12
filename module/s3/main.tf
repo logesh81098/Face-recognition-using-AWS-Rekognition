d2d46d8bb7db64574c8ba2b6a7c7b4d82dff4c5f
Logesh Shanmugavel <logesh81098@gmail.com>
2025-07-12 08:55:56 +0530

    Configured S3 bucket to trigger Faceprints Lambda function for each object creation

--- module/s3/main.tf
+++ module/s3/main.tf
@@ -11,3 +11,18 @@ resource "aws_s3_bucket" "source-bucket" {
     Project = "Recognizing-faces-using-AWS-Rekognition-service"
   }
 }
+
+
+####################################################################################################################################################################################
+#                                                                    S3 to trigger lambda
+####################################################################################################################################################################################
+
+#Configuring S3 to trigger Lambda Function
+
+resource "aws_s3_bucket_notification" "s3-trigger-lambda" {
+  bucket = aws_s3_bucket.source-bucket.bucket
+  lambda_function {
+    lambda_function_arn = var.rekognition-faceprints-function-arn
+    events = ["s3:ObjectCreated:*"]
+  }
+}
