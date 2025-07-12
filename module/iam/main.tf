72d3890d6c0d8b47806f91969668383378a1e92a
Logesh Shanmugavel <logesh81098@gmail.com>
2025-07-12 15:27:37 +0530

    Deployed IAM Role for Jenkins server

--- module/iam/main.tf
+++ module/iam/main.tf
@@ -179,3 +179,107 @@ resource "aws_iam_role_policy_attachment" "faceprints" {
   role = aws_iam_role.rekognition-faceprints-role.id
   policy_arn = aws_iam_policy.rekognition-faceprints-policy.arn
 }
+
+
+####################################################################################################################################################################################
+#                                                                          IAM Role
+####################################################################################################################################################################################
+
+#IAM Role for Jenkins Server
+
+resource "aws_iam_role" "jenkins-server-role" {
+  name = "Rekognition-Jenkins-Server-Role"
+  description = "IAM Role for Jenkins Server"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      }
+    }
+  ]
+}  
+EOF
+  tags = {
+    Name = "Rekognition-Jenkins-Server-Role"
+    Project = "Recognizing-faces-using-AWS-Rekognition-service"
+  }
+}
+
+
+####################################################################################################################################################################################
+#                                                                          IAM Policy
+####################################################################################################################################################################################
+
+#IAM Policy for Jenkins Server
+
+resource "aws_iam_policy" "jenkins-server-policy" {
+  name = "Rekognition-Jenkins-Server-policy"
+  description = "IAM Policy for Jenkins Server"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "CloudWatchLogGroup",
+      "Effect": "Allow",
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:*:*:*"
+    },
+    {
+      "Sid": "FullAccessDynamoDB",
+      "Effect": "Allow",
+      "Action": [
+        "dynamodb:*"
+      ],
+      "Resource": "arn:aws:dynamodb:*:*:*"
+    },
+    {
+      "Sid": "RekognitionIndexFace",
+      "Effect": "Allow",
+      "Action": [
+        "rekognition:*"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Sid": "S3PutSourceImage",
+      "Effect": "Allow",
+      "Action": [
+        "s3:ListBucket",
+        "s3:PutObject",
+        "s3:GetObject",
+        "s3:HeadObject"
+      ],
+      "Resource": [
+        "arn:aws:s3:::face-rekognition-source-bucket/*",
+        "arn:aws:s3:::face-rekognition-source-bucket"
+      ]
+    }
+  ]
+
+}  
+EOF
+  tags = {
+    Name = "Rekognition-Jenkins-Server-Policy"
+    Project = "Recognizing-faces-using-AWS-Rekognition-service"
+  }
+}
+
+
+####################################################################################################################################################################################
+#                                                                     IAM Role Policy Attachment
+####################################################################################################################################################################################
+
+resource "aws_iam_role_policy_attachment" "jenkins-server" {
+  role = aws_iam_role.jenkins-server-role.id
+  policy_arn = aws_iam_policy.jenkins-server-policy.arn
+}
