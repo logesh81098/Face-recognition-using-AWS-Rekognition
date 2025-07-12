2c11a3718c432edc281db832d9077a79706081b1
Logesh Shanmugavel <logesh81098@gmail.com>
2025-07-12 23:52:09 +0530

    Updated Jenkins server's UserData to install Jenkins

--- module/ec2-instance/main.tf
+++ module/ec2-instance/main.tf
@@ -42,5 +42,14 @@ resource "aws_instance" "rekognition-jenkins-server" {
   docker build -t logeshshanmugavel/face-rekognition-app .
   docker run -d -p 81:81 logeshshanmugavel/face-rekognition-app
   python3 upload-images-to-s3.py
+  sleep 10
+  sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+  sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+  sudo dnf install java-17-amazon-corretto -y
+  sudo dnf install jenkins -y
+  sudo systemctl enable jenkins
+  sudo systemctl start jenkins
+  sudo systemctl status jenkins
+  usermod -aG docker jenkins
   EOF
 }
