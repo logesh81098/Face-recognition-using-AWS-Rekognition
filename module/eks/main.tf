477100b4339b0f338fc98b21f4b85c3406b9384e
Logesh Shanmugavel <logesh81098@gmail.com>
2025-07-17 11:29:05 +0530

    Created Open ID Connect for Face Rekognition Cluster

--- module/eks/main.tf
+++ module/eks/main.tf
@@ -4,7 +4,7 @@
 
 #Face Rekognition EKS Cluster
 
-resource "aws_eks_cluster" "eks-cluster" {
+resource "aws_eks_cluster" "face-rekognition-cluster" {
   name = "Face-Rekognition-Cluster"
   version = var.cluster-version
   role_arn = var.cluster-iam-role
@@ -26,7 +26,7 @@ resource "aws_eks_cluster" "eks-cluster" {
 #EKS Node Group
 
 resource "aws_eks_node_group" "face-rekognition-node-group" {
-  cluster_name = aws_eks_cluster.eks-cluster.name
+  cluster_name = aws_eks_cluster.face-rekognition-cluster.name
   node_group_name = "Face-Rekognition-NodeGroup"
   node_role_arn = var.nodegroup-role
   subnet_ids = [ var.subnet-1, var.subnet-2 ]
@@ -46,3 +46,23 @@ resource "aws_eks_node_group" "face-rekognition-node-group" {
   }
   
 }
+
+####################################################################################################################################################################################
+#Attaching service account to K8S cluster so that Application can able to connect with AWS Services
+
+data "aws_eks_cluster" "face-rekognition-cluster-data" {
+  name = "Face-Rekognition-Cluster"
+  depends_on = [ aws_eks_cluster.face-rekognition-cluster ]
+}
+
+###################################################################################################################################################################################
+#                                                        EKS OpenID Provider
+##################################################################################################################################################################################
+
+#Creating OpenID connect provider, So that Containers in Pod in EKS Cluster can able to access AWS services
+
+resource "aws_iam_openid_connect_provider" "face-rekognition-openid-connect-provider" {
+  url = data.aws_eks_cluster.face-rekognition-cluster-data.identity[0].oidc[0].issuer
+  client_id_list = ["sts.amazonaws.com"]
+}
+
